d179b134879a0f521b316b6b13ac51d62a2f7af4
anshtandel12 <ansh.tandel@dcmail.ca>
2026-07-31 13:40:26 -0400

    added collision detection

--- Assignment 4/part 3/main.js
+++ Assignment 4/part 3/main.js
@@ -67,3 +67,19 @@ while (balls.length < 25) {
 
   balls.push(ball);
 }
+class Ball {
+  // …
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
+}
